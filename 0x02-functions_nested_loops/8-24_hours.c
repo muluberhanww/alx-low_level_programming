489e219cf02e81557666bccc8e3c1fe71d3e0c4e
muluberhanww <muluberhanw16@gmail.com>
2022-09-13 10:17:36 -0700

    task 8-2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,8 @@
 
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hour_max;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
 	hours_max = 58;
 	hours_tens = '0';
 	while (hours_tens < '3')
